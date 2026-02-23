0541fe112d586c0b87df1e41156a36c727f7e0c3
“PhungQuocThai” <phungquocthai.aeron.dev@gmail.com>
2026-02-24 02:18:07 +0700

    Setup project foundation architecture

---
+++ techstore-frontend/techstore-client/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://localhost:8888/techstore/api/v1',
+    imageUrl: 'http://localhost:8888/techstore/api/v1'
+};
---
+++ techstore-frontend/techstore-client/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:8888/techstore/api/v1',
+    imageProductUrl: 'http://localhost:8888/techstore/api/v1/file/media/download/img/products/',
+    imageAvatarUrl: 'http://localhost:8888/techstore/api/v1/file/media/download/img/users/',
+};
